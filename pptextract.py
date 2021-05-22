bfde255a56243b03f9917d978ff2e944781f4235
CrumblyLiquid <crumblyliquid@gmail.com>
2021-05-22 18:49:09 +0200

    Save files to .docx instead of .txt

--- pptextract.py
+++ pptextract.py
@@ -3,10 +3,15 @@
 import os
 from pathlib import Path
 from pptx import Presentation
+from docx import Document
+from docx.enum.text import WD_ALIGN_PARAGRAPH
+from docx.shared import Inches, Pt
 
 # Go through every .pptx file in that directory
 for filepath in os.listdir(Path(__file__).parent.absolute()):
     if filepath.endswith('.pptx'):
+        d = Document()
+        title_separator = "<title-separator>"
         p = Presentation(filepath)
         text = []
         previous = ""
@@ -30,10 +35,16 @@ for filepath in os.listdir(Path(__file__).parent.absolute()):
                             t = t.rstrip(" ").rstrip("\t")
                             # Only text with something in it
                             if (t != ""):
-                                parts.append("\t- " + t)
+                                parts.append(t)
             # For first slide only
             if (p.slides.index(slide) == 0 and title is not None):
-                text.append(f"----- {title} -----\n")
+                dtitle = d.add_paragraph()
+                dtitle.paragraph_format.alignment = WD_ALIGN_PARAGRAPH.CENTER
+                run = dtitle.add_run(title)
+                font = run.font
+                font.name = 'Calibri'
+                font.bold = True
+                font.size = Pt(20)
             # Skip slide
             elif (title in skips):
                 continue
@@ -41,7 +52,7 @@ for filepath in os.listdir(Path(__file__).parent.absolute()):
                 if (title is not None):
                     # Add title if it's a different than the last one
                     if (title != previous):
-                        text.append(title)
+                        text.append(title_separator+title)
                     # Remove \n if both slides have the same title
                     else:
                         # We remove the last item of the list which should be a ""
@@ -53,8 +64,23 @@ for filepath in os.listdir(Path(__file__).parent.absolute()):
                 # Add slide splitter
                 text.append("") # Will turn into \n
 
-        # Save text into a .txt file
-        txt_name = filepath.split("/")[-1].replace(".pptx", "") + ".txt"
+        for t in text:
+            if (t.startswith(title_separator)):
+                paragraph = d.add_paragraph()
+                run = paragraph.add_run(t.replace(title_separator, ""))
+                font = run.font
+                font.name = 'Calibri'
+                font.size = Pt(16)
+            elif (t == ""):
+                d.add_paragraph(t)
+            else:
+                paragraph = d.add_paragraph(style='List Bullet')
+                run = paragraph.add_run(t)
+                font = run.font
+                font.name = 'Calibri'
+                font.size = Pt(12)
+
+        # Save .docx file
+        txt_name = filepath.split("/")[-1].replace(".pptx", "") + ".docx"
         txt_path = str(Path(__file__).parent.absolute() / txt_name)
-        with open(txt_path, "w", encoding="utf-8") as fp:
-            fp.write("\n".join(text))
+        d.save(txt_path)
